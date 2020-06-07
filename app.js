8650b5904a9378dacecea9690e65dc7e1118ea7a
Tony Suriyathep <ts1176@gmail.com>
2020-06-07 10:20:04 -0700

    Added example.

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello World!")
+const http = require('http');
+const port = process.env.PORT || 3000
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<h1>Hello World which python</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
